6c37b3caca1544aed4602daaddb5d02c2e977db0
Cesar Camacho <cesarcamacho760@gmail.com>
2024-06-13 17:28:35 -0700

    add cursor and interaction to soundwave

--- app/components/SoundWave.tsx
+++ app/components/SoundWave.tsx
@@ -3,49 +3,90 @@ import React, { useEffect, useRef, useState } from "react";
 import { convertMillisToSeconds } from "@/lib/utils";
 import useMousePosition from "./hooks/useMousePosition";
 const SoundWave = ({ trackInfo }: { trackInfo: any }) => {
+  const soundDivisions = 25;
   const mousePosition = useMousePosition(true);
   const cursor = useRef(null);
   const box = useRef(null);
+  const [percentage, setPercentage] = useState(null);
+  const [timeSelect, setTimeSelect] = useState(null);
   const [soundArray, setSoundArray] = useState(
-    new Array(25)
+    new Array(soundDivisions)
       .fill(1)
-      .map((prev) => Math.floor(Math.random() * (128 - 20) + 20))
+      .map((prev) => Math.floor(Math.random() * (110 - 20) + 20))
   );
-  if (cursor.current) {
-    cursor.current.style.left =
-      mousePosition.x - box.current.getBoundingClientRect().left + "px";
-    console.log(cursor.current.style.left);
+  const rect = box.current?.getBoundingClientRect();
+
+  let sector = 0;
+  let inBound =
+    cursor.current &&
+    mousePosition.x > rect?.left &&
+    mousePosition.x < rect?.right &&
+    mousePosition.y < rect?.bottom &&
+    mousePosition.y > rect?.top;
+  if (inBound) {
+    sector = (mousePosition.x - rect?.left) / (rect?.width / 25);
+    console.log(sector);
+    console.log(percentage);
   }
 
   useEffect(() => {
-    var rect = box.current.getBoundingClientRect();
-    var x = mousePosition.x - rect.left; //x position within the element.
-    var y = mousePosition.y - rect.top; //y position within the element.
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
+    const handleResize = () => {
+      setTimeSelect((rect?.width * percentage) / 100);
+      console.log(timeSelect);
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
+
+  const handleTimeSelectClick = () => {
+    setTimeSelect(mousePosition.x - rect.left);
+    setPercentage(((mousePosition.x - rect.left) / rect.width) * 100);
+  };
 
   return (
     <div className="m-auto flex items-center justify-center ">
-      <p className="b">0:00 {JSON.stringify(mousePosition)}</p>
+      <p className="b">0:00</p>
       <div className="p-5  max-w-[700px]  relative">
         <div
           ref={box}
-          className=" h-32 flex gap-2 bg-slate-500 items-center z-10 relative justify-between"
+          onClick={() => handleTimeSelectClick()}
+          className=" h-32 flex gap-2 bg-slate-700 items-center z-10 relative justify-between"
         >
           {soundArray.map((single, index) => {
             return (
               <div
                 key={index}
-                style={{ height: `calc(${single}px)` }}
-                className={`w-[1vw] rounded-full bg-clip-content bg-white bg-opacity-50`}
+                style={{
+                  height: `calc(${single}px)`,
+                  backgroundColor: `${
+                    (!inBound && index < timeSelect / (rect?.width / 25)) ||
+                    index < sector
+                      ? "white"
+                      : "gray"
+                  }`,
+                }}
+                className={`w-[1vw] rounded-full bg-clip-content bg-slate-400 `}
               ></div>
             );
           })}
         </div>
-        <div className="absolute left-0 top-0  w-[200px] h-32 z-0 bg-red-500"></div>
+
         <div className="relative mt-5">
-          <div ref={cursor} className="absolute right-[16px]">
-            <div className="bg-slate-500 w-8 h-8 rounded-full relative ">
+          <div
+            ref={cursor}
+            style={{
+              left: inBound ? mousePosition.x - rect?.left : timeSelect + "px",
+            }}
+            className="absolute "
+          >
+            <div
+              style={{
+                backgroundColor: inBound ? "rgb(100 116 139)" : "white",
+              }}
+              className="bg-slate-500 w-8 h-8 rounded-full right-[16px] relative "
+            >
               <div
                 style={{
                   borderLeft: "16px solid transparent",
@@ -53,8 +94,16 @@ const SoundWave = ({ trackInfo }: { trackInfo: any }) => {
                   borderBottom:
                     "32px solid rgb(100 116 139 / var(--tw-border-opacity))",
                 }}
-                className="border-b-slate-500 w-0 h-0    bottom-[20px]  absolute"
+                className="border-b-slate-500 w-0 h-0   bottom-[20px]  absolute"
               ></div>
+              <p className="text-white absolute left-[40px] top-1">
+                {convertMillisToSeconds(
+                  inBound
+                    ? (mousePosition.x - rect?.left) /
+                        (rect.width / trackInfo.duration_ms)
+                    : timeSelect / (rect?.width / trackInfo.duration_ms)
+                )}
+              </p>
             </div>
           </div>
         </div>
